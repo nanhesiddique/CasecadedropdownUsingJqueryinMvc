07e808a71f0b391f2e9352da058607d3c46b48e8
Nanhe Siddique <nanhesiddi58@gmail.com>
2020-06-09 01:27:27 +0530

    CaseCadeDropDownList

---
+++ CasecadingDropdownList/Controllers/CaseCadeddlController.cs
@@ -0,0 +1,37 @@
+﻿using CasecadingDropdownList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasecadingDropdownList.Controllers
+{
+    public class CaseCadeddlController : Controller
+    {
+        // GET: CaseCadeddl
+
+        tutorialEntities db = new tutorialEntities();
+        public ActionResult Index()
+        {
+
+
+            ViewBag.CountryList = new SelectList(GetCountryList(), "CountryId", "CountryName");
+            return View();
+        }
+        public List<Country> GetCountryList()
+        {
+            List<Country> ct = db.Countries.ToList();
+            ViewBag.CountryList = new SelectList(ct, "CountryId", "CountryName");
+            return ct;
+        }
+        public ActionResult GetStateList(int CountryId)
+        {
+            List<State> st = db.States.Where(x => x.CountryId == CountryId).ToList();
+            ViewBag.StateOption = new SelectList(st,"StateId","StateName");
+            return PartialView("PartialViewOption");
+                
+        }
+
+    }
+}
---
+++ CasecadingDropdownList/Models/CountryStateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CasecadingDropdownList.Models
+{
+    public class CountryStateViewModel
+    {
+        public int CountryId { get; set; }
+        public int StateId { get; set; }
+    }
+}
